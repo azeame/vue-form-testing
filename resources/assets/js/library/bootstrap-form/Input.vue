dc92136d509a189410a9c5671982fb689e95f5fd
Jason Cook <jason@elegasoft.com>
2016-01-02 11:41:50 -0800

    first commit

---
+++ resources/assets/js/library/bootstrap-form/Input.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div id="input-{{label}}" class="input-group form-input-{{type}}">
+            <label for="label-{{label | lowercase}}">
+                {{label}}
+            </label>
+            <input name="{{label | lowercase}}" class="form-control" type="{{type}}">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['label','type']
+    }
+</script>
+
---
+++ resources/assets/js/main.js
@@ -0,0 +1,10 @@
+import FormInput from './library/bootstrap-form/Input.vue';
+
+var Vue = require('vue');
+
+new Vue({
+    el:'#app',
+    components:{
+        'form-input':FormInput
+    }
+})
